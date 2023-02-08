ec6d310333bb7241fc1de4a600a81d9fd77ed2a9
Antyss77 <fragnatiikpvpbest@gmail.com>
2023-02-08 22:39:27 +0100

    Initial Commit

---
+++ DesignAutoclick/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DesignAutoclick.ViewModels;
+
+public class MainWindowViewModel : ObservableObject {
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ DesignAutoclick/Views/MainWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace DesignAutoclick.Views;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+    
+    
+    private void HideButton_Click(object sender, RoutedEventArgs e) {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e) {
+        this.Close();
+    }
+
+    private void MaximizeButton_Click(object sender, RoutedEventArgs e) {
+        if (this.WindowState == WindowState.Maximized)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+        else
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+    }
+
+
+    private void Window_PointerPressed(object sender, PointerPressedEventArgs e) {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            this.BeginMoveDrag(e);
+        }
+    }
+}
